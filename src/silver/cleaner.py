ab09902cecf8985acb9c8531772d0141ebae7475
gavved <nearsightday@protonmail.com>
2026-03-16 15:58:33 +0100

    pulled appliance_type cleaning outside the numeric fields cleaning-loop

--- src/silver/cleaner.py
+++ src/silver/cleaner.py
@@ -29,15 +29,16 @@ def clean_event(raw_event: dict) -> dict:
             except ValueError:
                 # Sätter None value om det är en "SENSOR_OFFLINE" (Blir NULL i DB)
                 cleaned[field] = None
-        # 2) Tvätta Appliance Type
-        if "appliance_type" in cleaned and isinstance(cleaned["appliance_type"], str):
-            # Steg A: Standardisera grunden (små bokstäver, inga mellanslag, byt bindestreck till understreck)
-            base_cleaned = cleaned["appliance_type"].strip().lower().replace(" ", "_").replace("-", "_")
 
-            # Steg B: Slå upp i vår mapping.
-            # .get(key, default) betyder: "Hittar du felstavningen i ordboken? Ge mig rättningen.
-            # Annars, behåll ordet precis som det är (default)."
-            cleaned["appliance_type"] = APPLIANCE_MAPPING.get(base_cleaned, base_cleaned)
+    # 2) Tvätta Appliance Type
+    if "appliance_type" in cleaned and isinstance(cleaned["appliance_type"], str):
+        # Steg A: Standardisera grunden (små bokstäver, inga mellanslag, byt bindestreck till understreck)
+        base_cleaned = cleaned["appliance_type"].strip().lower().replace(" ", "_").replace("-", "_")
+
+        # Steg B: Slå upp i vår mapping.
+        # .get(key, default) betyder: "Hittar du felstavningen i ordboken? Ge mig rättningen.
+        # Annars, behåll ordet precis som det är (default)."
+        cleaned["appliance_type"] = APPLIANCE_MAPPING.get(base_cleaned, base_cleaned)
 
     # 3) Hanterar location och ger unknown istället för att vi skräpar ner med null values
     if not cleaned.get("location"):
