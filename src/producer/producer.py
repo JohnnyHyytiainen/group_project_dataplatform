9a7db0b40b5eace604f3d2fb80ee3e4d3c68cd66
Johnny Hyytiäinen <Johnny.Hyytiainen@gmail.com>
2026-03-19 08:44:48 +0100

    fix(bug): fixed bug in silver cleaner script. Now logic works

--- src/producer/producer.py
+++ src/producer/producer.py
@@ -166,7 +166,7 @@ try:
             producer.produce(TOPIC_NAME, value=json_payload, callback=delivery_report)
             producer.poll(0)
             # Tidsintervall för att kontrollera hur snabbt data ska genereras
-            time.sleep(30)
+            time.sleep(60)
 
 except KeyboardInterrupt:
     print("\nTurning off...")
